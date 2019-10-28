7c7e316ec2488c0f8b48682ce47767872b2d58a8
ace <ace@momiage.org>
2019-10-28 15:56:03 +0900

    update

---
+++ get_jetson_nano_thermal.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+CMD="cat "
+SYS_PATH='/sys/devices/virtual/thermal/thermal_zone'
+VAL_TYPES=('A0' 'CPU' 'GPU' 'PLL' 'PMIC-Die')
+
+function usage() {
+    echo "${1} type"
+    echo "Type is .."
+    for i in {0..4}; do
+        echo "${i} : ${VAL_TYPES[$i]}"
+    done
+    exit
+}
+
+[ ${#} -eq 1 ] || usage $0
+
+echo "$(cat ${SYS_PATH}${1}/temp) / 1000" | bc
